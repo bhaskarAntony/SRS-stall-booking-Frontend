448c8c989b3abc4e8a741fb20d0a619d8ba731b8
bhaskarAntony <avinash80692@gmail.com>
2025-12-19 10:05:00 +0530

    all files

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://srs-stalls-backend-1.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
